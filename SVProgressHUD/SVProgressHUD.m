2883a8c6d6bb8322b1285237a3f3b1f808b424da
sungwoo <sw.jang@ridi.com>
2016-12-08 12:35:29 +0900

    displatDurationForString 글자당 0.11초 추가로 수정

--- SVProgressHUD/SVProgressHUD.m
+++ SVProgressHUD/SVProgressHUD.m
@@ -1232,7 +1232,7 @@ static const CGFloat SVProgressHUDDefaultAnimationDuration = 0.15;
 #pragma mark - Getters
 
 + (NSTimeInterval)displayDurationForString:(NSString*)string {
-    return MIN((float)string.length * 0.06 + 0.5, [self sharedView].minimumDismissTimeInterval);
+    return MIN((float)string.length * 0.11 + 0.5, [self sharedView].minimumDismissTimeInterval);
 }
 
 - (UIColor*)foregroundColorForStyle {
